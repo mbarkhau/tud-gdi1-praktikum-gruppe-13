6516942bb9fc08f3ed1bff7af6a70c65aa106292
TeaAlk <TeaAlk@12b9761e-6e45-0410-8424-3f9a0b9a95ae>
2008-02-21 14:23:03 +0000

    Logic Expressions für HFSM-Logik

--- src/mis/gdi1lab07/student/gameBehaviour/logicExpressions/BallPassedByMe.java
+++ src/mis/gdi1lab07/student/gameBehaviour/logicExpressions/BallPassedByMe.java
@@ -6,6 +6,9 @@ import mis.gdi1lab07.student.gameData.GameEnv;
 
 public class BallPassedByMe<T extends GameEnv> extends BaseLogicExpression<T>
 		implements LogicExpression<T> {
+	
+	//Passender Spieer hat Ball erfolgreich gepasst, wenn Entfernung von 
+	//Spieler zu Ball größer als 3 Meter ist.
 
 	public BallPassedByMe(T env) {
 		super(env);
@@ -13,8 +16,7 @@ public class BallPassedByMe<T extends GameEnv> extends BaseLogicExpression<T>
 
 	@Override
 	public boolean eval(T env) throws LogExpException {
-		// TODO Auto-generated method stub
-		return false;
+		return env.getBall().getDistance() > 3;
 	}
 
 }
--- src/mis/gdi1lab07/student/gameBehaviour/logicExpressions/BallPassedToMe.java
+++ src/mis/gdi1lab07/student/gameBehaviour/logicExpressions/BallPassedToMe.java
@@ -2,17 +2,23 @@ package mis.gdi1lab07.student.gameBehaviour.logicExpressions;
 
 import mis.gdi1lab07.automaton.logic.LogExpException;
 import mis.gdi1lab07.student.gameData.GameEnv;
+import mis.gdi1lab07.student.gameData.GameMessages;
 
-public class BallPassedToMe<T extends GameEnv> extends BaseLogicExpression<T> {
+public class BallPassedToMe<T extends GameEnv> extends BaseLogicExpression<T> implements GameMessages {
 
+	//Stellt fest, dass ein Pass kommt, da ANNOUNCE_PASS empfangen wurde.
 	public BallPassedToMe(T env) {
 		super(env);
 	}
 
 	@Override
 	public boolean eval(T env) throws LogExpException {
-		// TODO Auto-generated method stub
-		return false;
+		if(env.receivedMessage(ANNOUNCE_PASS)) {
+			env.removeMessage(ANNOUNCE_PASS);
+			return true;
+		}
+		else
+			return false;
 	}
 
 }
