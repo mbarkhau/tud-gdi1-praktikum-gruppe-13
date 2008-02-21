3457d61f8c9a6236a511cf00f195b3f10563da95
mbarkhau <mbarkhau@12b9761e-6e45-0410-8424-3f9a0b9a95ae>
2008-02-21 20:34:29 +0000

    Flaggen

--- trunk/src/mis/gdi1lab07/student/gameData/FlagConstants.java
+++ trunk/src/mis/gdi1lab07/student/gameData/FlagConstants.java
@@ -1,125 +1,132 @@
 package mis.gdi1lab07.student.gameData;
 
+/** 
+ * Buchstabe 1 : Our / Their / Center
+ * Buchstabe 2 : Outer / Back / Goal / Penalty 
+ * Buchstabe 3 : Center / Left / Right  
+ */
 public interface FlagConstants {
 
 	public static int INVALD = 0;
-	
-	public static int T_L_50 = 1;
 
-	public static int T_L_40 = 2;
+	// Outer
 	
-	public static int T_L_30 = 3;
+	public static int O_O_R_50 = 1;
+
+	public static int O_O_R_40 = 2;
 	
-	public static int T_L_20 = 4;
+	public static int O_O_R_30 = 3;
 	
-	public static int T_L_10 = 5;
+	public static int O_O_R_20 = 4;
 	
-	public static int T_0 = 6;
+	public static int O_O_R_10 = 5;
 	
-	public static int T_R_50 = 7;
+//	public static int C_O_R_0 = 6;
 	
-	public static int T_R_40 = 8;
+	public static int T_O_R_50 = 7;
 	
-	public static int T_R_30 = 9;
+	public static int T_O_R_40 = 8;
 	
-	public static int T_R_20 = 10;
+	public static int T_O_R_30 = 9;
 	
-	public static int T_R_10 = 11;
+	public static int T_O_R_20 = 10;
+	
+	public static int T_O_R_10 = 11;
 
 	
-	public static int B_L_50 = 12;
+	public static int O_O_L_50 = 12;
 
-	public static int B_L_40 = 13;
+	public static int O_O_L_40 = 13;
 	
-	public static int B_L_30 = 14;
+	public static int O_O_L_30 = 14;
 	
-	public static int B_L_20 = 15;
+	public static int O_O_L_20 = 15;
 	
-	public static int B_L_10 = 16;
+	public static int O_O_L_10 = 16;
 	
-	public static int B_0 = 17;
+//	public static int C_O_L_0 = 17;
 	
-	public static int B_R_50 = 18;
+	public static int T_O_L_50 = 18;
 	
-	public static int B_R_40 = 19;
+	public static int T_O_L_40 = 19;
 	
-	public static int B_R_30 = 20;
+	public static int T_O_L_30 = 20;
 	
-	public static int B_R_20 = 21;
+	public static int T_O_L_20 = 21;
 	
-	public static int B_R_10 = 22;
+	public static int T_O_L_10 = 22;
 	
+	// behind goal
+	public static int O_B_R_30 = 23;
 	
-	public static int L_T_30 = 23;
+	public static int O_B_R_20 = 24;
 	
-	public static int L_T_20 = 24;
+	public static int O_B_R_10 = 25;
 	
-	public static int L_T_10 = 25;
-	
-	public static int L_0 = 26;
+//	public static int O_B_C_0 = 26;
 
-	public static int L_B_30 = 27;
+	public static int O_B_L_30 = 27;
 	
-	public static int L_B_20 = 28;
+	public static int O_B_L_20 = 28;
 	
-	public static int L_B_10 = 29;
+	public static int O_B_L_10 = 29;
 
 
-	public static int R_T_30 = 30;
+	public static int T_B_R_30 = 30;
 	
-	public static int R_T_20 = 31;
+	public static int T_B_R_20 = 31;
 	
-	public static int R_T_10 = 32;
+	public static int T_B_R_10 = 32;
 	
-	public static int R_0 = 33;
+//	public static int T_B_C_0 = 33;
 
-	public static int R_B_30 = 34;
+	public static int T_B_L_30 = 34;
 	
-	public static int R_B_20 = 35;
+	public static int T_B_L_20 = 35;
 	
-	public static int R_B_10 = 36;
+	public static int T_B_L_10 = 36;
 	
+	// außenfahnen
+	public static int O_O_L = 37;
 	
-	public static int L_T = 37;
+	public static int O_O_R = 38;
 	
-	public static int R_T = 38;
-	
-	public static int C_T = 39;
+	public static int C_O_R = 39;
 	
 	public static int C = 40;
 	
-	public static int C_B = 41;
+	public static int C_O_L = 41;
 
-	public static int L_B = 42;
+	public static int T_O_L = 42;
 	
-	public static int R_B = 43;
+	public static int T_O_R = 43;
+	
+	// torkasten
+	public static int O_P_R = 44;
+	
+	public static int O_P_C = 45;
+	
+	public static int O_P_L = 46;
 	
 	
-	public static int P_L_T = 44;
+	public static int T_P_R = 47;
 	
-	public static int P_L_C = 45;
+	public static int T_P_C = 48;
 	
-	public static int P_L_B = 46;
-	
-	
-	public static int P_R_T = 47;
-	
-	public static int P_R_C = 48;
-	
-	public static int P_R_B = 49;
+	public static int T_P_L = 49;
 
-	// Goal Theirs
-	public static int G_T_R = 50;
-	
-	public static int G_T_C = 51;
-	
-	public static int G_T_L = 52;
+	// Tor
 
-	// Goal Ours
-	public static int G_O_C = 53;
+	public static int O_G_C = 53;
 	
-	public static int G_O_R = 54;
+	public static int O_G_R = 54;
 	
-	public static int G_O_L = 55;
+	public static int O_G_L = 55;
+
+	public static int T_G_R = 50;
+	
+	public static int T_G_C = 51;
+	
+	public static int T_G_L = 52;
 	
 }
