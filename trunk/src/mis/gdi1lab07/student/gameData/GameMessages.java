bd4a0008b1dcbe10e9e5ef87315d00a2352c2b54
TeaAlk <TeaAlk@12b9761e-6e45-0410-8424-3f9a0b9a95ae>
2008-02-21 13:54:35 +0000

    Nachrichten der Spieler

---
+++ trunk/src/mis/gdi1lab07/student/gameData/GameMessages.java
@@ -0,0 +1,11 @@
+package mis.gdi1lab07.student.gameData;
+
+public interface GameMessages {
+
+	public static String REQUEST_ACCEPTION = "Nimm an"; //Fordert Passannahmen an
+	
+	public static String ACCEPT_PASS = "Angenommen"; //Nimmt Pass an
+	
+	public static String ANNOUNCE_PASS = "Pass kommt"; //Kündigt Pass an
+	
+}
